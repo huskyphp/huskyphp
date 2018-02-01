4fcddd621a3dfa6ece937e5f0172be25782c844c
Shivam Bansal <bshivam4@gmail.com>
2018-02-01 12:52:48 +0530

    index file + routes

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+date_default_timezone_set('Asia/Kolkata');
+setlocale (LC_TIME, 'fr_FR.utf8','fra');
+
+require __DIR__ . '/../vendor/autoload.php';
+$app = new \Slim\App;
+
+require __DIR__.'/../routes.php';
+
+$app->run();
